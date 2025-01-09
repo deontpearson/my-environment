916c1621f67f16b112ce13240cefb6b2e2892c1d
Deon Pearson <46675621+deontpearson@users.noreply.github.com>
2025-01-09 10:43:07 +0200

    feat: bring the repo up to date

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+ln -sf $HOME/workspace/dotfiles/gitignore $HOME/.gitignore
+ln -sf $HOME/workspace/dotfiles/gitconfig $HOME/.gitconfig
+ln -sf $HOME/workspace/dotfiles/zprofile $HOME/.zprofile
